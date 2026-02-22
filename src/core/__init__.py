996555699708b97d5b3532e9fe93513b7c5321f5
Hayato <hayato.fukami.0808@gmail.com>
2026-02-22 23:42:28 +0900

    Remove `database.py` and associated imports from `__init__.py`

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,14 +1,6 @@
 # src/core/__init__.py
 
 from src.core.config import *
-from src.core.database import (
-    init_db,
-    close_db,
-    WebSession,
-    get_session_by_sid,
-    create_session,
-    delete_session,
-)
 from src.core.crypto import encrypt, decrypt
 from src.core.dependencies import (
     sign_value,
